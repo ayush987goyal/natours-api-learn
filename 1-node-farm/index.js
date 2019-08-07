bf16f3898d5b9a395e62d8f70604073ad3939477
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-07 23:47:47 +0530

    Basic http server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const http = require('http');
+
+//////////////////////////////////////////
+////////// FILES
 
 // Blocking sync way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -9,19 +13,29 @@ const fs = require('fs');
 // console.log('File written');
 
 // Non-blocking, async way
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('Error!');
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('Error!');
 
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
 
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(data3);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('File written!');
-      });
-    });
-  });
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('File written!');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file!');
+
+//////////////////////////////////////////
+////////// SERVER
+const server = http.createServer((req, res) => {
+  res.end('Hello from server!');
 });
-console.log('Will read file!');
+
+server.listen(8000, '0.0.0.0', () =>
+  console.log('Listening to requests on port 8000')
+);
