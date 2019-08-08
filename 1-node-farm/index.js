27e9070f49f739dd08fca4d10dff65297d3e9f6e
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-08 20:57:54 +0530

    Add templates adnd replace with data

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -34,19 +34,56 @@ const url = require('url');
 //////////////////////////////////////////
 ////////// SERVER
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const temProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const server = http.createServer((req, res) => {
-  console.log(req.url);
+const replaceTemplate = (template, product) => {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+
+  return output;
+};
+
+const server = http.createServer((req, res) => {
   const pathname = req.url;
 
   switch (pathname) {
     case '/':
-    case '/overview':
-      res.end('This is the overview');
+    case '/overview': {
+      const cardsHtml = dataObj
+        .map(el => replaceTemplate(tempCard, el))
+        .join('');
+      const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+      res.writeHead(200, {
+        'Content-type': 'text/html'
+      });
+      res.end(output);
       break;
+    }
 
     case '/product':
       res.end('This is the PRODUCT');
