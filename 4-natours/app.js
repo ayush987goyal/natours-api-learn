089459fc91e8af30bd02bb54020e677de1fbfff2
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-11 09:10:09 +0530

    Add env

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -7,13 +7,12 @@ const userRouter = require('./Routes/userRoutes');
 const app = express();
 
 // Middlewares
-app.use(morgan('dev'));
-app.use(express.json());
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-app.use((req, res, next) => {
-  console.log('Hello from middle');
-  next();
-});
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,8 @@
+require('dotenv').config({ path: './config.env' });
+
 const app = require('./app');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port http://localhost:${PORT} ...`);
 });
