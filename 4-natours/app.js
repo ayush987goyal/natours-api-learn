3a60f3b821f7296ff63d3caf57ecc0169a79024e
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-11 07:34:39 +0530

    Add single tour fetch param

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -19,6 +19,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+
+  const tour = tours.find(t => t.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour
+    }
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = { id: newId, ...req.body };
@@ -28,7 +49,7 @@ app.post('/api/v1/tours', (req, res) => {
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
-    err => {
+    () => {
       res.status(201).json({
         status: 'success',
         data: {
