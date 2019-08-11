626b6373670c96e9414bcaa3cb862ad20aeade4b
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-11 17:48:51 +0530

    Update tourmodel and import using script

---
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -0,0 +1,45 @@
+require('dotenv').config({ path: `${__dirname}/../../config.env` });
+const fs = require('fs');
+const mongoose = require('mongoose');
+
+const Tour = require('../../models/tourModel');
+
+const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
