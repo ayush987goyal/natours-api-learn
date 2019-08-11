b4d9cc5ba60c6df3bb37ac1d839f5cb339a08fa4
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-11 14:50:01 +0530

    Connect to mdb using mongoose

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,6 +1,17 @@
 require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
 
 const app = require('./app');
+
+const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
