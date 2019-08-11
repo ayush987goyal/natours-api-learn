4be89849ff271d8ba3f29e1dcf012f01df79b3a1
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-11 07:40:54 +0530

    Add patch

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -60,6 +60,22 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated Tour Here>'
+    }
+  });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port http://localhost:${PORT} ...`);
