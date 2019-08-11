587556dd725c2530aac87e9106149efed20f3d7f
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-11 07:42:29 +0530

    Add delete placeholder

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -76,6 +76,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port http://localhost:${PORT} ...`);
