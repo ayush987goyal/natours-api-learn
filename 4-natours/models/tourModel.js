761c1672d0555366d36954cbb35034ac56891975
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-12 23:55:03 +0530

    Add document middleware

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const toursSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const toursSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -63,6 +65,15 @@ const toursSchema = new mongoose.Schema(
 toursSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
+
+toursSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// toursSchema.post('save', function(doc, next) {
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', toursSchema);
 
