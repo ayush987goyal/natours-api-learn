c74f9480e468e79086f86de970377f6000f254b0
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-12 22:34:24 +0530

    Add pagination

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -33,6 +33,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    // Execute
     const tours = await query;
 
     res.status(200).json({
