0423d2518717145d91f10c9c5bff191a7680da34
Ayush Goyal <ayush987goyal@gmail.com>
2019-08-14 00:01:29 +0530

    Add  unhanldedRejections handler

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -14,6 +14,15 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`App running on port http://localhost:${PORT} ...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
