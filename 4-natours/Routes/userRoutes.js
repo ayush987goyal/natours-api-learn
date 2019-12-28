0a2b1da786721ad58c803884e32f40d1a693a765
Ayush Goyal <ayush987goyal@gmail.com>
2019-12-28 09:20:29 +0530

    Add reset password token to model and add route

--- 4-natours/Routes/userRoutes.js
+++ 4-natours/Routes/userRoutes.js
@@ -8,6 +8,9 @@ const router = express.Router();
 router.route('/signup').post(authController.signup);
 router.route('/login').get(authController.login);
 
+router.route('/forgotPassword').post(authController.forgotPassword);
+router.route('/resetPassword').post(authController.resetPassword);
+
 router
   .route('/')
   .get(userController.getAllUsers)
--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -97,3 +97,16 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with that email address', 404));
+  }
+
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  console.log(resetToken);
+});
+
+exports.resetPassword = (req, res, next) => {};
