8d664dbcb0d6a2bcc3677e03fd509f52dede8c17
Ayush Goyal <ayush987goyal@gmail.com>
2019-12-28 10:15:52 +0530

    Add password reset func

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 
@@ -137,4 +138,30 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const hashedToken = crypto
+    .createHash('sha256')
+    .update(req.params.token)
+    .digest('hex');
+
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() }
+  });
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired.', 400));
+  }
+
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token
+  });
+});
--- 4-natours/utils/email.js
+++ 4-natours/utils/email.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const sendEmail = async options => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: +process.env.EMAIL_PORT,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD
