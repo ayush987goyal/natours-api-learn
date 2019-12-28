dcf5fd2408c575aeb8ead91ef87a8377f4eb0db6
Ayush Goyal <ayush987goyal@gmail.com>
2019-12-28 15:36:56 +0530

    Add sanitization, xss clean and hpp

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +32,26 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+// Sanitize data against NOSQL query injections
+app.use(mongoSanitize());
+
+// Sanitize data against XSS
+app.use(xss());
+
+// Prevent paramater pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 // Serve static files
 app.use(express.static(`${__dirname}/public`));
