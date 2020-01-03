f7c8c5b232a4ac8e80c810f3e13536b6f2410027
Ayush Goyal <ayush987goyal@gmail.com>
2020-01-03 21:16:19 +0530

    Add base pug template

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -69,7 +69,10 @@ app.use((req, res, next) => {
 
 // Routes
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Ayush'
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
