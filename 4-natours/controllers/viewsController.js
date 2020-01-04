058451894cbd5e98de8f6deda813e2f8c614f237
Ayush Goyal <ayush987goyal@gmail.com>
2020-01-04 09:27:57 +0530

    Ad mapbox for map

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -18,7 +18,7 @@ exports.getTour = catchAsync(async (req, res, next) => {
   });
 
   res.status(200).render('tour', {
-    title: tour.name,
+    title: `${tour.name} Tour`,
     tour
   });
 });
---
+++ 4-natours/public/js/mapbox.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-undef */
+
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYXl1c2g5ODdnb3lhbCIsImEiOiJjazR6MWU5ZmswNjVkM3FwOHdpNWZkaHc5In0.-cK7lVl6B6GiXqTIX8EGoA';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/ayush987goyal/ck4z1h9cb1yg21cmnls4zve6i',
+  scrollZoom: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(loc => {
+  // Create marker element
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom'
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100
+  }
+});
