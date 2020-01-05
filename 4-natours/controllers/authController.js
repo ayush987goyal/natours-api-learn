e8299cc1e4d91b204a4983820d44ef4ac92d8fda
Ayush Goyal <ayush987goyal@gmail.com>
2020-01-05 11:21:50 +0530

    Add passwordReset email

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -170,15 +170,8 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     'host'
   )}/api/v1/users/resetPassword/${resetToken}`;
 
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.
-  \nIf you didn't forget your password, please ignore this email.`;
-
   try {
-    // await sendEmail({
-    //   email: user.email,
-    //   subject: 'Your password reset token (valid for 10 min)',
-    //   message
-    // });
+    await new Email(user, resetURL).sendPasswordReset();
   } catch (err) {
     user.passwordResetToken = undefined;
     user.passwordResetExpires = undefined;
