131a6bae0a7308f2d434451a371c858f3f404b26
Ayush Goyal <ayush987goyal@gmail.com>
2020-01-05 10:34:49 +0530

    Add tour image upload func

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -42,21 +42,19 @@ const upload = multer({
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
-  const filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  req.file.filename = filename;
-
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`public/img/users/${filename}`);
+    .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
