a60c648e62887243e8b311be4567ca2aecf84f9b
Ayush Goyal <ayush987goyal@gmail.com>
2020-01-05 10:10:38 +0530

    Add file upload from ui

--- 4-natours/public/js/index.js
+++ 4-natours/public/js/index.js
@@ -36,9 +36,12 @@ if (userDataForm) {
   userDataForm.addEventListener('submit', e => {
     e.preventDefault();
 
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    updateSettings('data', { name, email });
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+
+    updateSettings('data', form);
   });
 }
 
